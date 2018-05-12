474711c8be78f3d343b02b0d735138fc6c9ef6cb
kimani-njoroge <kimani064@gmail.com>
2018-05-12 21:23:44 +0300

    replace total score with 0 if a player rolls a 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,22 @@
 var countTotal=0;
 var result;
-
+var diceNumber;
 var die = {
   sides: 6,
   roll: function(){
     var diceNumber = Math.floor(Math.random() * this.sides) + 1;
     countTotal+=diceNumber;
-    return diceNumber;
+    if (diceNumber ===1){
+      countTotal = 0;
+    } else if (diceNumber > 1) {
+      return diceNumber;
+    }
   }
 }
 function rollDie() {
   var result = die.roll();
   return result;
 }
-
 
 $(document).ready(function(){
   $("#register").submit(function(event){
@@ -29,7 +32,6 @@ $(document).ready(function(){
     event.preventDefault();
 
     var result = die.roll();
-
     $("#cur1").text(result);
     $("#tot1").text(countTotal.toString());
 
